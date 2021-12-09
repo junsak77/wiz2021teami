d5b285d7ba85f4d01df6b4215cb22833c1e1721b
YanaiShohei893 <zeal20410026@fsgcl.jp>
2021-12-09 15:38:47 +0900

    カテゴリ選択をいたらサブカテゴリが出てくる

--- main.py
+++ main.py
@@ -134,6 +134,7 @@ def handle_message(event):
             event.reply_token,
             TemplateSendMessage(alt_text='carousel template', template=message_template)
         )
+
     elif content in ['医療・保健・福祉関連']:
         carousel_columns = [
             CarouselColumn(
@@ -175,6 +176,261 @@ def handle_message(event):
                         label = '健康・生活',
                         data = 'callback',
                         text = '健康・生活'
+                    )
+                ]
+            )
+        ]
+        message_template = CarouselTemplate(columns=carousel_columns)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TemplateSendMessage(alt_text='carousel template', template=message_template)
+        )
+
+    elif content in ['震災・復旧・復興関連']:
+        carousel_columns = [
+            CarouselColumn(
+                text = '分野を選択してください',
+                title = '分野選択',
+                actions = [
+                    PostbackTemplateAction(
+                        label = '原発',
+                        data = 'callback',
+                        text = '原発'
+                    ),
+                    PostbackTemplateAction(
+                        label = '生活',
+                        data = 'callback',
+                        text = '生活'
+                    )
+                ]
+            ),
+            CarouselColumn(
+                text = '分野を選択してください',
+                title = '分野選択',
+                actions = [
+                    PostbackTemplateAction(
+                        label = '企業・経営',
+                        data = 'callback',
+                        text = '企業・経営'
+                    ),
+                    PostbackTemplateAction(
+                        label = '復興支援',
+                        data = 'callback',
+                        text = '復興支援'
+                    )
+                ]
+            )
+        ]
+        message_template = CarouselTemplate(columns=carousel_columns)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TemplateSendMessage(alt_text='carousel template', template=message_template)
+        )
+
+    elif content in ['生活関連']:
+        carousel_columns = [
+            CarouselColumn(
+                text = '分野を選択してください',
+                title = '分野選択',
+                actions = [
+                    PostbackTemplateAction(
+                        label = '事故',
+                        data = 'callback',
+                        text = '事故'
+                    ),
+                    PostbackTemplateAction(
+                        label = '生活・人間関係',
+                        data = 'callback',
+                        text = '生活・人間関係'
+                    ),
+                     PostbackTemplateAction(
+                        label = '情報関連',
+                        data = 'callback',
+                        text = '情報関連'
+                    )
+                ]
+            ),
+            CarouselColumn(
+                text = '分野を選択してください',
+                title = '分野選択',
+                actions = [
+                    PostbackTemplateAction(
+                        label = '食品・安全',
+                        data = 'callback',
+                        text = '食品・安全'
+                    ),
+                    PostbackTemplateAction(
+                        label = 'その他 (NPO法人)',
+                        data = 'callback',
+                        text = 'その他 (NPO法人)'
+                    )
+                ]
+            )
+        ]
+        message_template = CarouselTemplate(columns=carousel_columns)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TemplateSendMessage(alt_text='carousel template', template=message_template)
+        )
+
+    elif content in ['環境関連']:
+        carousel_columns = [
+            CarouselColumn(
+                text = '分野を選択してください',
+                title = '分野選択',
+                actions = [
+                    PostbackTemplateAction(
+                        label = '環境問題',
+                        data = 'callback',
+                        text = '環境問題'
+                    ),
+                    PostbackTemplateAction(
+                        label = '公害・廃棄物',
+                        data = 'callback',
+                        text = '公害・廃棄物'
+                    )
+                ]
+            ),
+            CarouselColumn(
+                text = '分野を選択してください',
+                title = '分野選択',
+                actions = [
+                    PostbackTemplateAction(
+                        label = '環境保全活動',
+                        data = 'callback',
+                        text = '環境保全活動'
+                    )
+                ]
+            )
+        ]
+        message_template = CarouselTemplate(columns=carousel_columns)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TemplateSendMessage(alt_text='carousel template', template=message_template)
+        )
+
+    elif content in ['産業・労働・就業関連']:
+        carousel_columns = [
+            CarouselColumn(
+                text = '分野を選択してください',
+                title = '分野選択',
+                actions = [
+                    PostbackTemplateAction(
+                        label = '労働環境',
+                        data = 'callback',
+                        text = '労働環境'
+                    ),
+                    PostbackTemplateAction(
+                        label = '経営',
+                        data = 'callback',
+                        text = '経営'
+                    ),
+                    PostbackTemplateAction(
+                        label = '産業',
+                        data = 'callback',
+                        text = '産業'
+                    )
+                ]
+            )
+        ]
+        message_template = CarouselTemplate(columns=carousel_columns)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TemplateSendMessage(alt_text='carousel template', template=message_template)
+        )
+
+    elif content in ['警察・犯罪関連']:
+        carousel_columns = [
+            CarouselColumn(
+                text = '分野を選択してください',
+                title = '分野選択',
+                actions = [
+                    PostbackTemplateAction(
+                        label = '安全相談',
+                        data = 'callback',
+                        text = '安全相談'
+                    ),
+                    PostbackTemplateAction(
+                        label = '交通安全',
+                        data = 'callback',
+                        text = '交通安全'
+                    )
+                ]
+            ),
+            CarouselColumn(
+                text = '分野を選択してください',
+                title = '分野選択',
+                actions = [
+                    PostbackTemplateAction(
+                        label = 'いじめ・子ども相談',
+                        data = 'callback',
+                        text = 'いじめ・子ども相談'
+                    )
+                ]
+            ),
+            CarouselColumn(
+                text = '分野を選択してください',
+                title = '分野選択',
+                actions = [
+                    PostbackTemplateAction(
+                        label = '犯罪関連',
+                        data = 'callback',
+                        text = '犯罪関連'
+                    )
+                ]
+            )
+        ]
+        message_template = CarouselTemplate(columns=carousel_columns)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TemplateSendMessage(alt_text='carousel template', template=message_template)
+        )
+    
+    elif content in ['パスポート関係 + 外国人向け']:
+        carousel_columns = [
+            CarouselColumn(
+                text = '分野を選択してください',
+                title = '分野選択',
+                actions = [
+                    PostbackTemplateAction(
+                        label = 'パスポート',
+                        data = 'callback',
+                        text = 'パスポート'
+                    ),
+                    PostbackTemplateAction(
+                        label = '外国人向け相談窓口',
+                        data = 'callback',
+                        text = '外国人向け相談窓口'
+                    )
+                ]
+            )
+        ]
+        message_template = CarouselTemplate(columns=carousel_columns)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TemplateSendMessage(alt_text='carousel template', template=message_template)
+        )
+
+    elif content in ['教育関連']:
+        carousel_columns = [
+            CarouselColumn(
+                text = '分野を選択してください',
+                title = '分野選択',
+                actions = [
+                    PostbackTemplateAction(
+                        label = '教育相談',
+                        data = 'callback',
+                        text = '教育相談'
+                    ),
+                    PostbackTemplateAction(
+                        label = '障がい児関連',
+                        data = 'callback',
+                        text = '障がい児関連'
+                    ),
+                    PostbackTemplateAction(
+                        label = '調査・文化財',
+                        data = 'callback',
+                        text = '調査・文化財'
                     )
                 ]
             )
